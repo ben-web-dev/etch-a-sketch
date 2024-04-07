6b87677da04bbc653d5c846b82d262e262f31cdd
ben-web-dev <mrbenrice@yahoo.com>
2024-04-07 13:17:59 +0100

    Colour button functionality and CSS/HTML changes - Added functionality to the colour select button using the JavaScript   colour picker and using an event listener to change the colour that   the user is using - Added a grid size slider with a label showing the size of te grid - Some minor CSS changes:   - Added a border radius to the buttons so they're more sleek   - Changed the colour that starts on page load to keep with the theme   - Changed size of the grid to not overflow onto the header and footer     whilst on a full computer screen - Title added and footer text added

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
 const gameContainer = document.getElementById('game-container');
 const colourButton = document.getElementById('select-ink');
+const gridSizeSlider = document.getElementById('grid-size');
+const gridSizeLabel = document.getElementById('grid-size-label');
 
+const INITIAL_GRID = 8;
 let gridSize = 8;
-gameContainer.style.gridTemplateRows = `repeat(${gridSize}, ${50/gridSize}vh)`
-gameContainer.style.gridTemplateColumns = `repeat(${gridSize}, ${50/gridSize}vh`
-let colourSelected = 'lightgreen';
+gameContainer.style.gridTemplateRows = `repeat(${gridSize}, ${40/gridSize}vw)`
+gameContainer.style.gridTemplateColumns = `repeat(${gridSize}, ${40/gridSize}vw`
+let colourSelected = 'bisque';
 let rainbowArray = ['red', 'orange', 'yellow', 
                     'green', 'blue', 'indigo', 'violet']
 let rainbow = false;
-createGrid(gridSize * gridSize);
+createGrid(INITIAL_GRID * INITIAL_GRID);
 colourTiles(colourSelected);
+
+gridSizeSlider.addEventListener('change', () => {
+    let value = gridSizeSlider.value
+    createGrid(value*value);
+    gameContainer.style.gridTemplateRows = `repeat(${value}, ${40/value}vh)`
+    gameContainer.style.gridTemplateColumns = `repeat(${value}, ${40/value}vh`
+    colourTiles(colourSelected);
+    gridSizeLabel.textContent = `Grid size: ${gridSizeSlider.value} x ${gridSizeSlider.value}`;
+})
 
 // Creating the tiles for etch-a-sketch
 function createGrid(pixels) {
+    gameContainer.innerHTML = '';
     for (let i = 0; i < pixels; i++) {
         const gameTile = document.createElement('div');
         gameTile.classList.add('game-tile');
@@ -47,7 +60,7 @@ function colourTiles(colourSelected) {
         })
     })
 }
-
+// Clear the game grid
 function clearGrid() {
     const tiles = document.querySelectorAll('.game-tile');
     console.log(tiles);
@@ -55,20 +68,27 @@ function clearGrid() {
         tile.style.backgroundColor = 'white';
     });
 }
-
+// White ink 
 function eraser() {
     rainbow = false;
     colourTiles('white');
 }
-
+// Black ink
 function blackenPen() {
     rainbow = false;
     colourTiles('black');
 }
-
+// Random colour (based on the rainbow array)
 function rainbowPen() {
     rainbow = true;
 }
+// Select a colour
+function selectColourPen() {
+    rainbow = false;
+    console.log(colourButton.value);
+    colourTiles(`${colourButton.value}`);
+}
+// Produces a random number for rainbow ink
 function randomNumber() {
     let randomNumber = Math.floor(Math.random() * 7)
     return randomNumber;
